15fb54907c4d1243f3fe47fe97387796c236bf8d
Mohammed <animeworldday2005@gmail.com>
2026-09-20 19:52:09 +0530

    f_sub.py

---
+++ mohammed/f_sub.py
@@ -0,0 +1,60 @@
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+from pyrogram import Client, filters, enums 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
+from info import *
+from .database import insert
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+async def not_subscribed(_, client, message):
+    user_id = int(message.from_user.id)
+    await insert(user_id)
+    if not FORCE_SUB:
+        return False
+    try:             
+        user = await client.get_chat_member(FORCE_SUB, message.from_user.id) 
+        if user.status == enums.ChatMemberStatus.BANNED:
+            return True 
+        else:
+            return False                
+    except UserNotParticipant:
+        pass
+    return True
+  
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+@Client.on_message(filters.private & filters.create(not_subscribed))
+async def forces_sub(client, message):
+    buttons = [[InlineKeyboardButton(text=" Join Update Channel ", url=f"https://t.me/{FORCE_SUB}") ]]
+    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
+    try:
+        silicon = await client.get_chat_member(FORCE_SUB, message.from_user.id)    
+        if silicon.status == enums.ChatMemberStatus.BANNED:                                   
+            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
+    except UserNotParticipant:                       
+        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
