51a2c8ef75257e5bc218bedae814e22c888c0ff6
Mohammed <animeworldday2005@gmail.com>
2026-09-20 19:50:03 +0530

    Script.py

---
+++ Script.py
@@ -0,0 +1,61 @@
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+import os
+
+class script(object):
+
+    START_TXT = """<b>Hᴇʟʟᴏ {}\n\n
+ɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ, ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ cʜᴀɴɴᴇʟ ᴀɴᴅ ᴇɴᴊᴏʏ
+
+‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Mr_Mohammed_29>ᴍᴏʜᴀᴍᴍᴇᴅ</a></b>
+"""
+
+    HELP_TXT = """•••[( 𝘎𝘦𝘵 𝘏𝘦𝘭𝘱 )]•••
+
+❗ 𝗔𝗹𝗲𝗿𝘁 ❗
+
+• Aᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ғᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.
+• Usᴇ ᴄᴏᴍᴍᴀɴᴅ ɢɪᴠᴇ ʙᴇʟᴏᴡ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ.
+• Tʜᴇsᴇ ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋ ɪɴ ᴄʜᴀɴɴᴇʟ.
+• Kᴇᴇᴘ ғɪʟᴇ ᴡɪᴛʜᴏᴜᴛ ғᴏʀᴡᴀʀᴅ ᴛᴀɢ.
+
+•> /set_cap - Sᴇᴛ Nᴇᴡ Cᴀᴘᴛɪᴏɴ Iɴ ʏᴏᴜʀ Cʜᴀɴɴᴇʟ
+•> /del_cap - Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ
+
+𝑭𝒐𝒓𝒎𝒂𝒕
+
+`{file_name}` = Oʀɪɢɪɴᴀʟ Fɪʟᴇ Nᴀᴍᴇ
+`{file_size}` = Oʀɪɢɪɴᴀʟ Fɪʟᴇ Sɪᴢᴇ 
+`{language}` = Lᴀɴɢᴜᴀɢᴇ Oғ Fɪʟᴇ Nᴀᴍᴇ
+`{year}` = Yᴇᴀʀ Oғ Fɪʟᴇ
+`{default_caption}` = Rᴇᴀʟ Cᴀᴘᴛɪᴏɴ Oғ Fɪʟᴇ.
+
+Eg:- `/set_cap
+{file_name}
+
+⚙️ Size » {file_size}
+"""
+
+    ABOUT_TXT = """<b>╔════❰ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ ʙᴏᴛ ❱═❍⊱❁
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼<b>📃 ʙᴏᴛ : <a href='https://t.me/Mr_Mohammed_29>Auto Caption</a></b>
+║┣⪼<b>👦 Cʀᴇᴀᴛᴏʀ : <a href='https://t.me/Mr_Mohammed_29>ᴍᴏʜᴀᴍᴍᴇᴅ</a></b>
+║┣⪼<b>🤖 Uᴘᴅᴀᴛᴇ : <a href='https://t.me/Aero_Unity'>ᴀᴇʀᴏ ᴜɴɪᴛʏ</a></b>
+║┣⪼<b>📡 Hᴏsᴛᴇᴅ ᴏɴ : <a href='https://t.me/Aero_Unity'>ᴀᴇʀᴏ ᴜɴɪᴛʏ</a></b>
+║┣⪼<b>🗣️ Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ3</b>
+║┣⪼<b>📚 Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ 2.11.6</b>
+║┣⪼<b>🗒️ Vᴇʀsɪᴏɴ : 2.0.8 [ᴍᴏsᴛ sᴛᴀʙʟᴇ]<b>
+║╰━━━━━━━━━━━━━━━➣
+╚══════════════════❍⊱❁</b>"""
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
