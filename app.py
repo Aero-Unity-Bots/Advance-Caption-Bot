f262da6d1a44540aaa27d7eec6bc0aa368a05328
Aero-Unity-Bots <aerounity2005@gmail.com>
2026-09-20 22:30:10 +0530

    app.py

--- app.py
+++ app.py
@@ -5,24 +5,52 @@
 # Support : @Coders_Grp 
 # ------------------------ #
 
+import threading
+
 from flask import Flask
+
+from bot import Bot
+
 
 app = Flask(__name__)
 
+
+# ------------------------ #
+# HOME
+# ------------------------ #
 
 @app.route("/")
 def hello_world():
     return "ᴍᴏʜᴀᴍᴍᴇᴅ ᴅᴇᴠᴇʟᴏᴘᴇʀ"
 
+
 # ------------------------ #
-# Don't Remove My Credits
-# Owner: @Mr_Mohammed_29
-# Updates: @Aero_Unity 
-# Support : @Coders_Grp 
+# START TELEGRAM BOT
 # ------------------------ #
 
-if __name__ == "__main__":
-    app.run()
+def start_bot():
+
+    try:
+        print("🚀 Starting Telegram Bot...")
+
+        bot = Bot()
+        bot.run()
+
+    except Exception as e:
+        print(f"❌ Telegram Bot Error: {e}")
+
+
+# ------------------------ #
+# RUN BOT IN BACKGROUND
+# ------------------------ #
+
+bot_thread = threading.Thread(
+    target=start_bot,
+    daemon=True
+)
+
+bot_thread.start()
+
 
 # ------------------------ #
 # Don't Remove My Credits
