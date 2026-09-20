9423c90d5a51bd55aff32bc323a2d312944ce4ef
Mohammed <animeworldday2005@gmail.com>
2026-09-20 19:39:02 +0530

    info.py

---
+++ info.py
@@ -0,0 +1,55 @@
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+from os import environ, getenv
+import re
+import os
+
+id_pattern = re.compile(r"^.\d+$")
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
+
+ADMIN = int(getenv("ADMIN", ""))
+MOHAMMED_PIC = os.environ.get("MOHAMMED_PIC", "https://telegra.ph/file/1d504928e362876977269-fdfcffde7ab2ca4ed0.jpg")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = str(getenv("API_HASH", ""))
+BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+MONGO_DB = str(getenv("MONGO_DB", ""))
+DEF_CAP = str(
+    getenv(
+        "DEF_CAP",
+        "<b>File Name:- `{file_name}`\n\n{file_size}</b>",
+    )
+)
+
+# ------------------------ #
+# Don't Remove My Credits
+# Owner: @Mr_Mohammed_29
+# Updates: @Aero_Unity 
+# Support : @Coders_Grp 
+# ------------------------ #
