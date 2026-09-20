736b77741d522b696234d73dd4f00c8f7f9bcf66
Aero-Unity-Bots <aerounity2005@gmail.com>
2026-09-20 23:08:20 +0530

    app.py

--- app.py
+++ app.py
@@ -1,60 +1,38 @@
-# ------------------------ #
-# Don't Remove My Credits
-# Owner: @Mr_Mohammed_29
-# Updates: @Aero_Unity 
-# Support : @Coders_Grp 
-# ------------------------ #
+
 
 import threading
+import traceback
 
 from flask import Flask
 
 from bot import Bot
 
-
-app = Flask(__name__)
-
-
-# ------------------------ #
-# HOME
-# ------------------------ #
+app = Flask(name)
 
 @app.route("/")
 def hello_world():
-    return "ᴍᴏʜᴀᴍᴍᴇᴅ ᴅᴇᴠᴇʟᴏᴘᴇʀ"
-
-
-# ------------------------ #
-# START TELEGRAM BOT
-# ------------------------ #
+return "ᴍᴏʜᴀᴍᴍᴇᴅ ᴅᴇᴠᴇʟᴏᴘᴇʀ"
 
 def start_bot():
 
-    try:
-        print("🚀 Starting Telegram Bot...")
+try:
+    print("🚀 Starting Telegram Bot...")
 
-        bot = Bot()
-        bot.run()
+    bot = Bot()
 
-    except Exception as e:
-        print(f"❌ Telegram Bot Error: {e}")
+    print("🔌 Telegram Bot Instance Created")
 
+    bot.run()
 
-# ------------------------ #
-# RUN BOT IN BACKGROUND
-# ------------------------ #
+except Exception as e:
+    print(f"❌ Telegram Bot Error: {e}")
+    traceback.print_exc()
 
 bot_thread = threading.Thread(
-    target=start_bot,
-    daemon=True
+target=start_bot,
+daemon=True,
+name="TelegramBotThread"
 )
 
 bot_thread.start()
 
-
-# ------------------------ #
-# Don't Remove My Credits
-# Owner: @Mr_Mohammed_29
-# Updates: @Aero_Unity 
-# Support : @Coders_Grp 
-# ------------------------ #
